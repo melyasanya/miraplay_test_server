2965b2b7fd32cd1c898dedf21083ee89976aa0b2
melyasanya <werbolom@gmail.com>
2023-11-14 16:31:14 +0200

    new try

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const { DB_HOST } = require("./config");
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
-    console.log("Database connection successful");
+    app.listen(PORT);
+    console.log(`Database connection successful, Listening on ${PORT}}`);
   })
   .catch((error) => {
     console.log(error.message);
