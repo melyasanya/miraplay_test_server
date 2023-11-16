e405655f9f82fd48bbab3e3c2a7888061717e7ad
melyasanya <werbolom@gmail.com>
2023-11-16 22:36:21 +0200

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const usersRouter = require("./routes/api/users");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
